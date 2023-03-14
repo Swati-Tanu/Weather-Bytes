a4b738482d1d9181fd8f79b388878c15d8da5cb7
Swati Tanu <swatitani01@gmail.com>
2023-03-14 20:53:02 +0530

    updated

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.static(""))
+
+//Routing
+app.get("", (req,res) => {
+    res.send("Welcome!")
+})
+
+app.get("/about", (req,res) => {
+    res.send("About Us")
+})
+
+app.get("/weather", (req,res) => {
+    res.send("Weather App")
+})
+
+app.get("*", (req,res) => {
+    res.send("404 Error Page")
+})
+
+app.listen(port, ()=> {
+    console.log((`Listening at ${port} port`));
+})
