182a15d4afa9e69572c4d5573e2c56e260c3dbbc
Swati Tanu <swatitani01@gmail.com>
2023-04-06 14:52:23 +0530

    connected frontend to backend

--- src/app.js
+++ src/app.js
@@ -1,30 +1,44 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const hbs = require("hbs");
 const port = process.env.PORT || 8000;
 
 //Static Path: We are connecting the static page to express here
 console.log(path.join(__dirname, "../public")); //C:\Users\swati\OneDrive\Desktop\Weather-App\public                                
 
-const static_path = path.join(__dirname, "../public") //address of index.html
+// Set the path to the views directory
+const template_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials')
 
+// Set the view engine
+app.set('view engine', 'hbs');
+app.set('views',template_path);
+hbs.registerPartials(partials_path);
+// app.set('views', template_path)
+
+const static_path = path.join(__dirname, "../public") //address of index.html
 app.use(express.static(static_path))
+
+
 
 //Routing
 app.get("/", (req,res) => {
-    res.send("Welcome to Weather App (Backend Side)!")
+    res.render("index")
 })
 
 app.get("/about", (req,res) => {
-    res.send("About Us")
+    res.render("about")
 })
 
 app.get("/weather", (req,res) => {
-    res.send("Weather App")
+    res.render("weather")
 })
 
 app.get("*", (req,res) => {
-    res.send("404 Error Page")
+    res.render("error", {
+        errorMsg: "Opps! Page not Found."
+    })
 })
 
 app.listen(port, ()=> {
